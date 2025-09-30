66187b57a9aab98f63a6e28c78862025faf2c5e9
lenomicheal <lenomicheal8@gmail.com>
2025-09-30 17:53:30 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -116,4 +116,4 @@ def index():
     return send_from_directory(app.template_folder, 'signup.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) 
