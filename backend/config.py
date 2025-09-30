0250f2249687662ef78a1489ef8f83f16ed54852
lenomicheal <lenomicheal8@gmail.com>
2025-09-30 10:41:42 +0530

    files added

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:RtpPVIoJgYiOLIKYXxJztvtpdAKNREpb@mainline.proxy.rlwy.net:11936/flask_app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = 'uploads'
+    ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
+
+
